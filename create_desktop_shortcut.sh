43dbabe7b0c5127484da388e4133e6e1759035ea
David Ibarra <david.ibarral0798@gmail.com>
2026-06-13 21:53:10 +0200

    activate .venv for run project

--- create_desktop_shortcut.sh
+++ create_desktop_shortcut.sh
@@ -6,6 +6,7 @@ PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 # Ruta al escritorio del usuario
 DESKTOP_FILE="$HOME/Desktop/KioskoEcoLuna.desktop"
 
+VENV_DIR=".venv"
 # Crear el contenido del archivo .desktop
 cat > "$DESKTOP_FILE" << EOF
 [Desktop Entry]
@@ -13,7 +14,7 @@ Version=1.0
 Type=Application
 Name=Kiosko EcoLuna
 Comment=Inicia el servidor NiceGUI y abre Chromium en modo kiosko
-Exec=bash -c "cd $PROJECT_DIR && python3 src/web_app/main.py & sleep 4 && chromium-browser --kiosk --window-position=0,0 --noerrdialogs --disable-infobars --no-first-run --incognito http://localhost:8000 --new-window --window-position=1024,0 http://localhost:8000/admin"
+Exec=bash -c "cd $PROJECT_DIR && source $VENV_DIR/bin/activate && python3 src/web_app/main.py & sleep 4 && chromium-browser --kiosk --window-position=0,0 --noerrdialogs --disable-infobars --no-first-run --incognito http://localhost:8000 --new-window --window-position=1024,0 http://localhost:8000/admin"
 Terminal=true
 Icon=$PROJECT_DIR/media/logo_slogan.png
 Categories=Utility;Application;
